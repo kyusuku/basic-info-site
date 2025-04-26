8f72844ed0abeab05f63fc497ed9dd04438a7dea
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-26 21:20:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    let fileName = "." + q.pathname + ".html";
+    if (q.pathname === "/") {
+      fileName = "./index.html";
+    } else if (
+      fileName !== "./contact-me.html" &&
+      fileName !== "./about.html"
+    ) {
+      fileName = "./404.html";
+    }
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
